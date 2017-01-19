3451d616df28e37821f699d7b5738e227fd33a9f
zangshayang1 <zangshayang1@hotmail.com>
2017-01-18 23:37:15 -0800

    indentation

--- hadoop-n-docker/3-project-AutoCompletion/NGram/src/Driver.java
+++ hadoop-n-docker/3-project-AutoCompletion/NGram/src/Driver.java
@@ -20,12 +20,12 @@ public class Driver {
 		conf1.set("noGram", args[2]);
 		
 		//First Job 
-	    Job job1 = Job.getInstance(conf1);
-	    job1.setJobName("NGram");
-	    job1.setJarByClass(Driver.class);
+	        Job job1 = Job.getInstance(conf1);
+	        job1.setJobName("NGram");
+	        job1.setJarByClass(Driver.class);
 
-	    job1.setMapperClass(NGramLibraryBuilder.NGramMapper.class);
-	    job1.setReducerClass(NGramLibraryBuilder.NGramReducer.class);
+	        job1.setMapperClass(NGramLibraryBuilder.NGramMapper.class);
+	        job1.setReducerClass(NGramLibraryBuilder.NGramReducer.class);
 
 		job1.setOutputKeyClass(Text.class);
 		job1.setOutputValueClass(IntWritable.class);
@@ -35,39 +35,40 @@ public class Driver {
 
 		TextInputFormat.setInputPaths(job1, new Path(args[0]));
 		TextOutputFormat.setOutputPath(job1, new Path(args[1]));
-	    job1.waitForCompletion(true);
-            //$ hadoop com.java.sun.tools.javac.Main *.java -> compile in hadoop
-            //$ jar cf ngram.jar *.class -> create file.jar to run
-            //$ hadoop jar ngram.jar Driver input /output 2 3 4
-            //2 -> ngram_size
-            //3 -> threshold: if the word appearing frequency is lower than threshold it will be left out
-            //4 -> following_word_size
+	    
+  	        job1.waitForCompletion(true);
+                //$ hadoop com.java.sun.tools.javac.Main *.java -> compile in hadoop
+                //$ jar cf ngram.jar *.class -> create file.jar to run
+                //$ hadoop jar ngram.jar Driver input /output 2 3 4
+                //2 -> ngram_size
+                //3 -> threshold: if the word appearing frequency is lower than threshold it will be left out
+                //4 -> following_word_size
 
-	    //Second Job 
-	    Configuration conf2 = new Configuration();
-	    conf2.set("threashold", args[3]);
-	    conf2.set("n", args[4]);
-	    DBConfiguration.configureDB(conf2,
+	        //Second Job 
+	        Configuration conf2 = new Configuration();
+	        conf2.set("threashold", args[3]);
+	        conf2.set("n", args[4]);
+	        DBConfiguration.configureDB(conf2,
 	    	     "com.mysql.jdbc.Driver",   // java database connectivity driver class
 	    	     "jdbc:mysql://17.245.76.166:8889/test", // host ip port
 	    	     "root",    // user name
 	    	     ""); //password
 		
-	    Job job2 = Job.getInstance(conf2);
-	    job2.setJobName("LanguageModel");
-	    job2.setJarByClass(Driver.class);
+	        Job job2 = Job.getInstance(conf2);
+	        job2.setJobName("LanguageModel");
+	        job2.setJarByClass(Driver.class);
 	    
-	    job2.addArchiveToClassPath(new Path("/mysql/mysql-connector-java-5.0.8-bin.jar"));
-            // jar stands for java archive file, which packs all the classes and metadata that make the API
-	    job2.setMapOutputKeyClass(Text.class);
-	    job2.setMapOutputValueClass(Text.class);
-	    job2.setOutputKeyClass(Text.class);
-	    job2.setOutputValueClass(NullWritable.class);
+	        job2.addArchiveToClassPath(new Path("/mysql/mysql-connector-java-5.0.8-bin.jar"));
+                // jar stands for java archive file, which packs all the classes and metadata that make the API
+	        job2.setMapOutputKeyClass(Text.class);
+	        job2.setMapOutputValueClass(Text.class);
+	        job2.setOutputKeyClass(Text.class);
+	        job2.setOutputValueClass(NullWritable.class);
 
-	    job2.setMapperClass(LanguageModel.Map.class);
-	    job2.setReducerClass(LanguageModel.Reduce.class);
+	        job2.setMapperClass(LanguageModel.Map.class);
+	        job2.setReducerClass(LanguageModel.Reduce.class);
 
-	    job2.setInputFormatClass(TextInputFormat.class);
+	        job2.setInputFormatClass(TextInputFormat.class);
 		job2.setOutputFormatClass(DBOutputFormat.class);
 	    
 		DBOutputFormat.setOutput(
@@ -76,7 +77,7 @@ public class Driver {
 			     new String[] { "starting_phrase", "following_word", "count" }   //table columns
 			     );
 		
-	    //Path name for this job should match first job's output path name
+  	        //Path name for this job should match first job's output path name
 		TextInputFormat.setInputPaths(job2, new Path(args[1]));
 		System.exit(job2.waitForCompletion(true)?0:1);
 
